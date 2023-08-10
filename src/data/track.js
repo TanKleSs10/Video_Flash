d7de366d816163dc0fef097141a87d7d0132ea91
Diego Meza <diegoerik_mh@hotmail.com>
2023-08-09 19:16:37 -0600

    se modifican generos de los track

--- src/data/track.js
+++ src/data/track.js
@@ -18,7 +18,7 @@ export const tracks = [
         img: 'https://catalogo-videoflash.web.app/assets/img/track2.jpg', 
         bpm: 88,
         price: 250,
-        tag: 'Trap',
+        tag: 'Rap',
         duracion: '03:00',
         licencia: 'MP3 Lease'
     },
@@ -29,7 +29,7 @@ export const tracks = [
         img: 'https://catalogo-videoflash.web.app/assets/img/track4.jpg', 
         bpm: 84,
         price: 190,
-        tag: 'Chill',
+        tag: 'Rap',
         duracion: '03:14',
         licencia: 'MP3 Lease'
     },
@@ -40,7 +40,7 @@ export const tracks = [
         img: 'https://catalogo-videoflash.web.app/assets/img/track1.jpg', 
         bpm: 92,
         price: 225,
-        tag: 'reggeton',
+        tag: 'Rap',
         duracion: '03:01',
         licencia: 'MP3 Lease'
     }
